d5a0c243e9d3accaa8691e999763e47cfe7cfc3e
demetSanz <elias@rural.com.ar>
2021-04-30 19:03:21 -0300

    actualizacion formularios

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const publicPath = path.resolve(__dirname, './public');
+const { get } = require('http');
 
 app.use(express.static(publicPath));
 
@@ -9,6 +10,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 });
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+});
 
 app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));
 
