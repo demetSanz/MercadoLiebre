ee8484549ddc58b6030f3ffaa5b60c74e6ae497a
demetSanz <elias@rural.com.ar>
2021-05-01 11:05:31 -0300

    termino clase 6 de formularios

--- app.js
+++ app.js
@@ -14,5 +14,9 @@ app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'))
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+});
+
 app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));
 
